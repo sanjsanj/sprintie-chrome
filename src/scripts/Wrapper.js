3f03acb568891caddd1924806b65531f3dd59ab2
Magnus Holm <mail@magnusholm.com>
2018-02-28 13:46:14 +0000

    Add tabs

--- src/scripts/Wrapper.js
+++ src/scripts/Wrapper.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 const Wrapper = () => {
-    return <div></div>
+    return (
+    <div>
+        <Tabs>
+            <TabList>
+                <Tab>Team 2</Tab>
+                <Tab>Team 2</Tab>
+            </TabList>
+            
+            <TabPanel>
+                <h2>Any content 1</h2>
+            </TabPanel>
+            <TabPanel>
+                <h2>Any content 2</h2>
+            </TabPanel>
+        </Tabs>
+    </div>
+    )
 }
 
 export default Wrapper
