fa54efc8bd5b6d79fa1ebc4d760e7315d9a1ceba
Sanjay Purswani <sanjsanj@hotmail.com>
2018-02-28 16:32:11 +0000

    start setting up storage

--- src/scripts/components/Wrapper.js
+++ src/scripts/components/Wrapper.js
@@ -1,3 +1,5 @@
+/* global chrome */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
@@ -6,20 +8,35 @@ import AddPerson from './AddPerson';
 import Person from './Person';
 
 const Wrapper = () => {
+    const tabs = ['Team Ginevra', 'Team Magnus'].map(team => {
+        return (
+            <Tab><input type="text" value={team} /></Tab>
+        )
+    });
+
+    // function addTeam() {
+    //     chrome.storage.sync.set({'Teams': ['Name your team']}, () => {
+    //         chrome.storage.sync.get('Teams', (team) => {
+    //             console.log(team);
+    //         })
+    //     });
+    // }
+
     return (
     <div>
         <Tabs>
             <TabList>
-                <Tab>Team 1</Tab>
-                <Tab>Team 2</Tab>
+                {tabs}
             </TabList>
 
             <TabPanel>
-                <AddPerson/>
                 <Option/>
+                <AddPerson/>
             </TabPanel>
             <TabPanel>
                 <h2>Any content 2</h2>
+                <Option/>                
+                <AddPerson/>                
             </TabPanel>
         </Tabs>
     </div>
